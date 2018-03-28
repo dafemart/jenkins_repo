a43a59a833f66a14b14c8d80b22823a7c375c0a3
Daniel <dafemart@ucsc.edu>
2018-03-28 14:51:02 -0400

    Update LoginServlet.java

--- TRMS_WORKING/src/servlets/LoginServlet.java
+++ TRMS_WORKING/src/servlets/LoginServlet.java
@@ -9,11 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet{
 
-
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 4286940923785367241L;
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException
